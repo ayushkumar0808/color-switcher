102e26ba765af5ab4cb5e765bc34c70d361c1d9b
Ayush Kumar <kayush3647@gmail.com>
2026-09-17 00:19:05 +0530

    add

---
+++ script.js
@@ -0,0 +1,4 @@
+let applyBtn = document.querySelector("#applyBtn");
+let randomBtn = document.querySelector("#randomBtn");
+let currentColor = document.querySelector("#currentColor");
+let text = document.querySelector("#text");
