cf516ef8094d1001ca7f27f0f71c6294b38c4e4c
Ayush Kumar <kayush3647@gmail.com>
2026-09-17 01:06:19 +0530

    add

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ let applyBtn = document.querySelector("#applyBtn");
 let randomBtn = document.querySelector("#randomBtn");
 let currentColor = document.querySelector("#currentColor");
 let text = document.querySelector("#text");
+let container = document.querySelector(".container");
+
+const colorChange = (color) => {
+  container.style.backgroundColor = color;
+  currentColor.innerText = color;
+};
+
+const applyBtnHandle = () => {
+  console.log(text.value);
+  colorChange(text.value);
+};
+applyBtn.addEventListener("click", applyBtnHandle);
+
+const randomBtnHandle = () => {
+  console.log("random btn");
+};
+randomBtn.addEventListener("click", randomBtnHandle);
